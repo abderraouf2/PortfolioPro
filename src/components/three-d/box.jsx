828816f1d76c5c3c4dc4e75455b80403a3043905
abderraouf2 <79739572+abderraouf2@users.noreply.github.com>
2022-05-11 10:05:55 +0100

    threeD element canged

--- src/components/three-d/box.jsx
+++ src/components/three-d/box.jsx
@@ -4,19 +4,19 @@ import React from 'react';
 // import { useLoader } from 'react-three-fiber';
 // import { TextureLoader } from 'three';
 // import Raouf from './raouf.png';
-import { MeshWobbleMaterial } from '@react-three/drei';
+// import { MeshWobbleMaterial } from '@react-three/drei';
 
 export default function Box() {
   // const colorMap= useLoader(TextureLoader,Raouf)
   return (
-    <mesh >
-      <torusKnotBufferGeometry attach='geometry' args={[1]} />
-      <meshNormalMaterial attach='material' color="#0F5257"  />
-      <MeshWobbleMaterial factor={2} 
-        color='#0F5257'
-        attach='material'
-        speed={1}
-      />
+    <mesh rotation={[5,2,5]}  >
+      <dodecahedronBufferGeometry attach='geometry' args={[1]} />
+      <meshNormalMaterial 
+        attach='material' 
+        color="#0F5257" 
+        />
+      
+      
     </mesh>
   )
 }
