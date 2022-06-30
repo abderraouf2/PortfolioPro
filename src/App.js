54524dcd1e7e7a90d043daf60d545e3548dbf16f
abderraouf2 <79739572+abderraouf2@users.noreply.github.com>
2022-06-30 10:17:57 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 import { Switch,Route } from "react-router-dom";
 
-import Loader from'./components/loader/Loader'
+// import Loader from'./components/loader/Loader'
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe/AboutMe';
 
@@ -10,20 +10,22 @@ import AboutMe from './pages/AboutMe/AboutMe';
 
 function App() {
   // const [loadded, setloadded] = useState(false);
-  const [loader,setLoader] = useState(true);
+  // const [loader,setLoader] = useState(true);
   // const ImgLoadded = () =>{
   //   setloadded(true)
   // }
-  useEffect(()=>{
-      setTimeout(()=>{
-        setLoader(false)
-      },4)
-  },[])
-  return loader ? (
-    <Loader />
-  ) 
+  // useEffect(()=>{
+  //     setTimeout(()=>{
+  //       setLoader(false)
+  //     },4)
+  // },[])
+  return   (
+  // loader ? (
+  //   <Loader />
+  // ) 
   
-  : (
+  // : 
+
         <div className='app'>
           <Switch>
             <Route exact path='/' >
